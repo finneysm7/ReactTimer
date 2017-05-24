4e5156799c044fee737e8e321b28a902a2b95f5a
Sean Finney <mcfinneys@gmail.com>
2017-05-23 22:18:57 -0700

    Add Navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var {Link} = require('react-router')
+
+var Countdown = (props) => {
+  return (
+    <div>
+      <p>Countdown.jsx Rendered</p>
+    </div>
+  )
+};
+
+module.exports = Countdown;
